2d4e94b215f0caffa13036fad31fa536f1eec8dc
Kimchu16 <kimberlylibranza@gmail.com>
2024-03-26 23:54:04 +0000

    Finished Sliding text animatioin class and implemented it in my own scene

--- java/src/C22386221/KimsVisual.java
+++ java/src/C22386221/KimsVisual.java
@@ -13,7 +13,8 @@ public class KimsVisual extends Visual{
     float x2;
     float x3;
     int fontSize = 128;
-    SlidingText bling;
+    SlidingText bling, bang, bang2;
+    SlidingText rBling, rBang, rBang2;
     
     public KimsVisual(song s, PFont font){
         this.s = s;
@@ -23,7 +24,14 @@ public class KimsVisual extends Visual{
         x = w;
         x2 = w;
         x3 = w;
-        bling = new SlidingText(s,"Test", w, h/12, 10, 0);
+
+        bling = new SlidingText(s,"Bling", font, x, h/12, 200, 0);
+        bang = new SlidingText(s, "Bang", font, x2, h/12, 200, 394);
+        bang2 = new SlidingText(s, "Bang", font, x3, h/12, 200, (394 + 389));
+
+        rBling = new SlidingText(s,"Bling", font, 0, h - h/9, 200, x - (394 + 389));
+        rBang = new SlidingText(s, "Bang", font, 0, h - h/9, 200, x - 394);
+        rBang2 = new SlidingText(s,"Bang", font, 0, h - h/9, 200, x);
     }
     
     public void play(){
@@ -38,14 +46,20 @@ public class KimsVisual extends Visual{
         s.rect(0, h, w, -h/5);
         
         if (m >= 1000){
-            UpperTextAnimation();
+            //UpperTextAnimation();
+            bling.SlideLeft();
+            bang.SlideLeft();
+            bang2.SlideLeft();
+
+            rBling.SlideRight();
+            rBang.SlideRight();
+            rBang2.SlideRight();
         }
         
-        bling.Render();
-        //System.out.println(s.textWidth("Bling"));
+        //System.out.println(s.textWidth("Bang"));
     }
     
-    public void UpperTextAnimation(){
+    /*public void UpperTextAnimation(){
         float y = h/12;
 
         s.textSize(fontSize);    
@@ -79,5 +93,5 @@ public class KimsVisual extends Visual{
             x3-=200;
         }
         
-    }
+    }*/
 }
--- java/src/C22386221/SlidingText.java
+++ java/src/C22386221/SlidingText.java
@@ -2,39 +2,58 @@ package C22386221;
 
 import ie.tudublin.Visual;
 import main.song;
+import processing.core.PFont;
 
 public class SlidingText extends Visual{
     song s;
     String text;
+    PFont font;
     float x,y;
     float speed;
     float end;
     
-    public SlidingText(song s,String text, float x, float y, float speed, float end){
+    public SlidingText(song s,String text, PFont font, float x, float y, float speed, float end){
         this.s = s;
         this.text = text;
+        this.font = font;
         this.x = x;
         this.y = y;
         this.speed = speed;
         this.end = end;
     }
 
-    public void Render(){
+    public void SlideLeft(){
         s.textSize(128);    
         s.fill(255);
+        s.textFont(font);
         s.textAlign(LEFT,CENTER);
 
         s.text(text,x,y);
+        //Checks if x position overlaps the end location and stops, if not move left
         if(x - end < speed){
             x = end;
         }else{
             x -= speed;
         }
-        
     }
+
+    public void SlideRight(){
+        s.textSize(128);    
+        s.fill(255);
+        s.textFont(font);
+        s.textAlign(RIGHT,CENTER);
+
+        s.text(text,x,y);
+        //Checks if x position overlaps the end location and stops, if not move right
+        if(x + speed > end){
+            x = end;
+        }else{
+            x += speed;
+        }
+    }
+
 }
 
 //TODO pass in speed variable (float), *slowly accelerate/decelerate?
-//TODO change the x from the start to the end
 /*TODO if the x is in front of the end reduce the current x by speed, otherwise
 increase the x */
