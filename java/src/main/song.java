225152bc67ed0c2cf53e848c1c02a1ad66c33e72
Aidan Mcphee <aidanmcpheecompsci@gmail.com>
2024-04-16 16:48:14 +0100

    added subtitles to main song

--- java/src/main/song.java
+++ java/src/main/song.java
@@ -1,15 +1,19 @@
 package main;
 
 import C22300773.AidansVisual;
+import C22300773.subtitles.SubtitleReader;
 import C22386221.KimsVisual;
 import C22408466.EilishsVisual;
 import ie.tudublin.Visual;
 import processing.core.PFont;
+import C22300773.subtitles.Subtitle;
 
-public class song extends Visual{
+public class Song extends Visual{
     KimsVisual Kim; 
     AidansVisual Aidan;
     EilishsVisual Eilish;
+    SubtitleReader sr;
+    Controller c;
 
     public void settings(){
         size(1920, 1080, P3D);
@@ -25,41 +29,43 @@ public class song extends Visual{
         startMinim();
         loadAudio("data/Creepy Nuts - Bling-Bang-Bang-Born.mp3");
         getAudioPlayer().play();
-        PFont font = createFont("Cabazon.otf", 200);
-        PFont calibri = createFont("calibri-regular.ttf", 100);
+        PFont font = createFont("fonts/Cabazon.otf", 200);
+        PFont calibri = createFont("fonts/calibri-regular.ttf", 100);
         //pg = createGraphics(1600, 900, P3D);
+        c = new Controller(this);
 
-        //Kim = new KimsVisual(this.g, font);
+        Kim = new KimsVisual(this.g, font);
         Eilish = new EilishsVisual(this.g);
         Aidan = new AidansVisual(this.g, calibri);
+        sr = new SubtitleReader("java/data/subtitles/subs.srt");
     }
 
-    public void keyPressed(){
-        if(keyCode == ' ') {
-            if (getAudioPlayer().isPlaying()) {
-                getAudioPlayer().pause();
-            } else {
-                getAudioPlayer().play();
-            }
-        }
-        if(keyCode == '0'){
-            getAudioPlayer().rewind();
-        }
+    public void keyPressed() {
+        c.handleKeyPress(keyCode);
     }
 
     private void playMusicVideo() {
         //Aidan.SpinningSphere(width, height, 1.0f);
         //String[] vars = {"Width", "height", "Amplitude"};
         //String[] vals = {Integer.toString(width), Integer.toString(height), Float.toString(getSmoothedAmplitude())};
-        //Kim.play();
+        Kim.play();
+        Subtitle sub = sr.getCurrentSubtitle(getAudioPlayer().position() / 1000);
+
+        if (sub != null) {
+            String text = sub.getText();
+            textAlign(CENTER, CENTER);
+            textSize(64);
+            text(text, width/2, height/2);
+        }
+
         //Eilish.setMouseX(mouseX);
-        Eilish.play();
+        //Eilish.play();
         //Aidan.printVars(vars, vals);
         //Aidan.SpinningSphere(100, 100, 50);
         //Aidan.printVars(vars, vals);
     }
     
-    public void draw(){
+    public void draw() {
         background(0);
         playMusicVideo();
     }
