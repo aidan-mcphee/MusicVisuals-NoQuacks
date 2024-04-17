e33fe7e1f2b89075c9c436ccea1afbedb34ed118
Jana <janrheiasy@gmail.com>
2024-04-17 14:58:17 +0100

    Flipped terrain to the top

--- java/src/C22363431/JanasVisual.java
+++ java/src/C22363431/JanasVisual.java
@@ -43,9 +43,9 @@ public class JanasVisual extends Visual{
         strokeWeight(1);
         noFill();
         pushMatrix();
-        translate(width/2, height/2); //Object of affection position
+        translate(width/2, height*0.8f); //Object of affection position
         rotateX(PI/3);
-        translate(-w/2, -h/2); // Camera 
+        translate(-w/2, -h/12); // Camera 
         flying -= 0.05f;
 
 
@@ -67,7 +67,36 @@ public class JanasVisual extends Visual{
             }
             endShape();
         }
+    
+        popMatrix();
 
+        pushMatrix();
+        translate(width/2, 0); //Object of affection position
+        scale(1, -1);
+        rotateX(PI/3);
+        translate(-w/2, -h/4); // Camera
+        flying -= 0.05f;
+
+
+        float y2offset = flying;
+        for (int x = 0; x < rows; x++) {
+            float x2offset = 0;
+            for (int y = 0; y < cols; y++) {
+                terrain[y][x] = map(noise(x2offset,y2offset), 0, 1, -500*amp, 500*amp);
+                x2offset += 0.2f;
+            }
+            y2offset += 0.2f;
+        }
+
+        for (int y = 0; y < rows-1; y++) {
+            beginShape(TRIANGLE_STRIP);
+            for (int x = 0; x < cols; x++) {
+                vertex(x*scale,y*scale, terrain[x][y]);
+                vertex(x*scale,(y+1)*scale, terrain[x][y+1]);
+            }
+            endShape();
+        }
+    
         popMatrix();
     }
 }
