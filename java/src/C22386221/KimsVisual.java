8cfc7ab6d24ce8615745e4c5390797eeecb78445
Kimchu16 <kimberlylibranza@gmail.com>
2024-04-23 19:01:51 +0100

    Added a checkered background

--- java/src/C22386221/KimsVisual.java
+++ java/src/C22386221/KimsVisual.java
@@ -17,9 +17,11 @@ public class KimsVisual extends Visual{
     int fontSize = 128;
     SlidingText bling, bang, bang2;
     SlidingText rBling, rBang, rBang2;
-    DancingStickman stickman;
+    DancingStickman stickman, stickman2;
     PImage[] stickmanImages;
-    MagicCircle magicCircle;
+    MagicCircle magicCircle, magicCircle2;
+    int numSquares;
+    int squareSize;
     
     public KimsVisual(PGraphics g, PFont font, PImage[] stickmanImg){
         this.g = g;
@@ -28,9 +30,12 @@ public class KimsVisual extends Visual{
         this.font = font;
         this.stickmanImages = stickmanImg;
 
+
         x = w;
         x2 = w;
         x3 = w;
+        numSquares = 5;
+        squareSize = (int) max(w, h) / numSquares;
 
         bling = new SlidingText(this.g,"Bling", font, x, h/12, 200, 0);
         bang = new SlidingText(this.g, "Bang", font, x2, h/12, 200, 394);
@@ -40,22 +45,35 @@ public class KimsVisual extends Visual{
         rBang = new SlidingText(this.g, "Bang", font, 0, h - h/9, 200, x - 394);
         rBang2 = new SlidingText(this.g,"Bang", font, 0, h - h/9, 200, x);
 
-        stickman = new DancingStickman(this.g, w/8,h/8, stickmanImg);
-        magicCircle = new MagicCircle(this.g, w / 2, h / 2 + 50, -100, 300, PI / 3);
+        stickman = new DancingStickman(this.g, w/2 + 50,h/2.5f, stickmanImg);
+        stickman2 = new DancingStickman(this.g, w/4 + 50,h/2.5f, stickmanImg);
+        magicCircle = new MagicCircle(this.g, w / 2, h / 2 + 50, -100, 400, PI / 3);
+        magicCircle2 = new MagicCircle(this.g, w / 3, h / 3 + 50, -180, 300, PI / 3);
     }
     
     public void play(float smoothAmp){
         int m = millis();
         colorMode(RGB);
-        //background(m % 255);
-        background(255, 204, 0);
+        background(0);
 
-        //black bars
-        fill(0);
-        rect(0, 0, w, h/5);
-        rect(0, h, w, -h/5);
+        noStroke();
+        pushMatrix();
+        translate(w/2, h/2, -600);
+        translate(-numSquares * squareSize / 2, -numSquares * squareSize / 2);
+        for (int i = 0; i < numSquares; i++) {
+            for (int j = 0; j < numSquares; j++) {
+                
+                if ((i + j) % 2 == 0) {
+                    fill(0);
+                } else {
+                    fill(255, 204, 0);
+                }
+                rect(j * squareSize, i * squareSize, squareSize, squareSize);
+            }
+          }
+        popMatrix();
         
-        if (m >= 1000){
+        if (second() % 2 == 0){
             bling.SlideLeft();
             bang.SlideLeft();
             bang2.SlideLeft();
@@ -67,9 +85,11 @@ public class KimsVisual extends Visual{
 
         magicCircle.setAmplitude(smoothAmp);
         magicCircle.render();
+        magicCircle2.setAmplitude(smoothAmp);
+        magicCircle2.render();
 
-        //stickman.render();
+        stickman.render();
+        stickman2.render();
         
-        //System.out.println(textWidth("Bang"));
     }
 }
